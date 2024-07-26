a01b1ed0e82555d757d9dca2620b86264a0ce2ce
Diyana Halim <sitinurdiyana98.dh@gmail.com>
2024-07-26 22:07:16 +0800

    Changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// fonts
+import "./assets/fonts/LagencyfreeRegular.otf";
+import "./assets/fonts/LagencyfreeRegular.woff";
+import "./assets/fonts/LagencyfreeRegular.woff2";
+import "./assets/fonts/Maisondeartisan.otf";
+import "./assets/fonts/Maisondeartisan.woff";
+import "./assets/fonts/Maisondeartisan.woff2";
+
 import App from './App.js';
 import reportWebVitals from './reportWebVitals.js';
 import { BrowserRouter } from 'react-router-dom';
